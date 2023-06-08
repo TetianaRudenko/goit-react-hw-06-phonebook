8fef70d5e0554baa9a8bcf1cf095fb7e11bb1375
TetianaRudenko <fyilibratgif@gmail.com>
2023-06-08 13:38:27 +0300

    write store, selectors, contacts-/filter-slice
    use Redux Toolkit

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,12 +1,71 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+//import { rootReducer } from "./reducer";
 
-const increment = createAction('counter/increment');
-console.log(increment());
 
-const myReducer = createReducer(0, {})
+import {  contactsReducer, } from "./contactsSlice";
+import { filterReducer } from "./filterSlice";
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
 
-export const store = configureStore({
-  reducer: {
-    myValue: myReducer,
-  },
-})
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export { store };
+
+//    import { createStore } from "redux";
+//    import { devToolsEnhancer } from "@redux-devtools/extension";
+//    //import { statusFilter } from "./constants";
+//    
+//    import { rootReducer } from "./reducer";
+//    
+//    
+//    // Початкове значення стану Redux для кореневого редюсера,
+//    // якщо не передати параметр preloadedState.
+//    //   const initialState = {
+//    //     contacts: [
+//    //       { id: 0, text: "Learn HTML and CSS", completed: true },
+//    //       { id: 1, text: "Get good at JavaScript", completed: true },
+//    //       { id: 2, text: "Master React", completed: false },
+//    //       { id: 3, text: "Discover Redux", completed: false },
+//    //       { id: 4, text: "Build amazing apps", completed: false },
+//    //     ],
+//    //     filter: {
+//    //       status: statusFilter.all,
+//    //     },
+//    //   };
+//    //   
+//    //   // Поки що використовуємо редюсер який
+//    //   // тільки повертає отриманий стан
+//    //   const rootReducer = (state = initialState, action) => {
+//    //     return state;
+//    //   };
+//    
+//    // Створюємо розширення стора, щоб додати інструменти розробника
+//    const enhancer = devToolsEnhancer();
+//    
+//    export const store = createStore(rootReducer, enhancer);
+//    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
